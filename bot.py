138b110d28ca9139a0b10456eb3974142226962d
costa <constantinos@kamvysis.com>
2021-10-08 21:54:18 +0100

    read some coinbase balances

--- bot.py
+++ bot.py
@@ -1,5 +1,23 @@
 import ccxt
+import settings
 
-exchange = ccxt.coinbasepro()
+exchange = ccxt.coinbasepro({
+    'apiKey': settings.COINBASE_PRO_KEY,
+    'secret': settings.COINBASE_PRO_SECRET,
+    'password': settings.COINBASE_PRO_PASSWORD
+})
 
 print(exchange)
+
+markets = exchange.load_markets()
+
+symbol = "ETH/USD"
+ticker=exchange.fetch_ticker(symbol)
+
+
+
+balance = exchange.fetch_balance()
+
+print(balance['total']['GBP'])
+
+print(balance['total']['ETH'])
