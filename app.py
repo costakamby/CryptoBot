49bb958555899df8d0fb2eda76f2ad2f033cc29d
costa <constantinos@kamvysis.com>
2021-10-09 19:33:56 +0100

    list of cryptos to choose from

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from binance.client import Client
+import settings,csv
+
 app = Flask(__name__)
+
+client = Client(settings.BINANCE_KEY,settings.BINANCE_SECRET)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    account = client.get_account()
+    balances = account['balances']
+
+    exchange_info = client.get_exchange_info()
+    
+    symbols = exchange_info['symbols']
+
+    return render_template('index.html',balances=balances,symbols=symbols)
 
 
-@app.route('/buy')
+@app.route('/buy',methods=['POST'])
 def buy():
+    print(request.form)
     return 'buy'
 
 @app.route('/sell')
